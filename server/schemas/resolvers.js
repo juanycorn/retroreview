9ab1a3d86cedf2835c7433ee41a00684b52671b8
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-05-22 19:57:57 -0700

    todos

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,21 @@
+const { query } = require("express");
 const {Save, User} = require("../models");
+const { Query } = require("mongoose");
 //const { signToken, AuthenticationError } = require('../utils/auth'); to be implemented
 
+const resolvers = {
+    Query: {
+        // find User req context
+
+        // findbyPk the save 
+    },
+    Mutatuoin: {
+        // create User returns a token
+
+        // updateUser reqcontext
+
+        // create a Save
+
+        //login returns a token
+    }
+}
