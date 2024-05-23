b20f4264dc96654909999438ac80c85e88e439d0
juanycorn <jaunido@gmail.com>
2024-05-22 20:51:04 -0700

    added background login and signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes
 import ApolloProviderWrapper from './apollo';
-import Items from './components/Games';
 import About from './components/About';
 import Contact from './components/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './components/Login';
 import Games from './components/Games';
+import SignUp from './components/SignUp';
 import './index.css';
 import './components/Header.css';
 import './components/Footer.css';
@@ -15,15 +15,16 @@ import './components/Footer.css';
 const App = () => (
   <ApolloProviderWrapper>
     <Router>
-      <div className="App">
+      <div className="parallax-bg">
         <Header />
-        <main style={{ padding: '20px', flex: '1' }}>
+        <main className='content' style={{ padding: '20px', flex: '1' }}>
           <Routes> {/* Use Routes instead of Switch */}
-            <Route path="/" element={<Items />} />
+            <Route path="/" element={""} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path='/login' element={<Login />} />
-            <Route path='/Games' element={<Games />} />
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/games' element={<Games />} />
           </Routes>
         </main>
         <Footer />
