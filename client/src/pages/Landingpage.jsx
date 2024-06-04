42fa81bc3061f8f8950e721ecb22b1389ef26e8f
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-06-03 19:41:30 -0700

    small cahnges

--- client/src/pages/Landingpage.jsx
+++ client/src/pages/Landingpage.jsx
@@ -31,7 +31,7 @@ const LandingPage = () => {
     <div className="landing-page">
       <Container className="welcome-container">
         <Header as="h1" textAlign="center" className="welcome-header">
-          Welcome to <Image src="/assets/retrocade.png" alt="Retrocade Logo" size="Large" />
+          Welcome to <Image src="public/assets/retrocade.png" alt="Retrocade Logo" size="Large" />
         </Header>
       </Container>
       <Container className="carousel-container">
